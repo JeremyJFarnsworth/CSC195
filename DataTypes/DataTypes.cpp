d03b6719e7f190cab8df79caa275f5b3a28622dd
JeremyJFarnsworth <jdog1123@gmail.com>
2025-04-02 10:07:59 -0600

    Payroll assignment completed

--- DataTypes/DataTypes.cpp
+++ DataTypes/DataTypes.cpp
@@ -2,10 +2,68 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Data Types Program\n";
+    string name;
+    char initial;
+    short age;  // Declare age
+    bool isAdult;
+    int zipcode;
+    float wage;
+    short daysWorked;
+    float hoursWorkedPerDay[7];
+
+    const float TAX = 0.1f;
+
+    // Get user input for age first
+    cout << "Enter age: ";
+    cin >> age;
+
+    // Now we can check if the user is an adult
+    isAdult = (age > 18 && age < 99);
+    cout << (isAdult ? "Valid age. You are an adult.\n" : "Invalid age. You are not an adult.\n");
+
+    cout << "Enter first name: ";
+    cin >> name;
+
+    cout << "Enter initial of last name: ";
+    cin >> initial;
+
+    cout << "Enter zipcode: ";
+    cin >> zipcode;
+
+    cout << "Enter hourly wage: ";
+    cin >> wage;
+
+    cout << "Enter number of days worked (max 7): ";
+    cin >> daysWorked;
+
+    float totalHours = 0; // Initialize total hours to 0
+
+    for (int i = 0; i < daysWorked; i++) {
+        cout << "Enter hours worked for day " << (i + 1) << ": ";
+        cin >> hoursWorkedPerDay[i];
+
+        totalHours += hoursWorkedPerDay[i];  // Accumulate total hours
+    }
+
+    float grossIncome = totalHours * wage;
+    float taxAmount = grossIncome * TAX;
+    float netIncome = grossIncome - taxAmount;
+
+    cout << "\nEmployee Payroll Summary\n";
+    cout << "-------------------------\n";
+    cout << "Name: " << name << " " << initial << ".\n";
+    cout << "Age: " << age << " (Adult: " << (isAdult ? "Yes" : "No") << ")\n";
+    cout << "Zipcode: " << zipcode << "\n";
+    cout << "Hourly Wage: $" << wage << " per hour\n";
+    cout << "Total Hours Worked: " << totalHours << " hours\n";
+    cout << "Gross Income: $" << grossIncome << "\n";
+    cout << "Tax Amount: $" << taxAmount << "\n";
+    cout << "Net Income: $" << netIncome << "\n";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
